634c73191dcce293ed53e3103f2b481d67b2dad4
AlexGrinch <grinchuk.alexey@gmail.com>
2018-04-16 17:31:45 +0300

    added utils: atari wrappers, efficient replay buffer

---
+++ utils.py
@@ -0,0 +1,341 @@
+"""
+    This file is copied/apdated from https://github.com/berkeleydeeprlcourse/homework/tree/master/hw3
+"""
+
+import numpy as np
+import random
+from collections import namedtuple, deque
+import gym
+from gym import spaces
+from PIL import Image
+
+####################################################################################################
+######################################## Experience Replay #########################################
+####################################################################################################
+
+def sample_n_unique(sampling_f, n):
+    """Helper function. Given a function `sampling_f` that returns
+    comparable objects, sample n such unique objects.
+    """
+    res = []
+    while len(res) < n:
+        candidate = sampling_f()
+        if candidate not in res:
+            res.append(candidate)
+    return res
+
+class ReplayBuffer(object):
+    def __init__(self, size, frame_history_len):
+        """This is a memory efficient implementation of the replay buffer.
+        The sepecific memory optimizations use here are:
+            - only store each frame once rather than k times
+              even if every observation normally consists of k last frames
+            - store frames as np.uint8 (actually it is most time-performance
+              to cast them back to float32 on GPU to minimize memory transfer
+              time)
+            - store frame_t and frame_(t+1) in the same buffer.
+        For the tipical use case in Atari Deep RL buffer with 1M frames the total
+        memory footprint of this buffer is 10^6 * 84 * 84 bytes ~= 7 gigabytes
+        Warning! Assumes that returning frame of zeros at the beginning
+        of the episode, when there is less frames than `frame_history_len`,
+        is acceptable.
+        Parameters
+        ----------
+        size: int
+            Max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+        frame_history_len: int
+            Number of memories to be retried for each observation.
+        """
+        self.size = size
+        self.frame_history_len = frame_history_len
+
+        self.next_idx      = 0
+        self.num_in_buffer = 0
+
+        self.obs      = None
+        self.action   = None
+        self.reward   = None
+        self.done     = None
+        
+        self.transition = namedtuple('Transition', ('s', 'a', 'r', 's_', 'done'))
+
+    def can_sample(self, batch_size):
+        """Returns true if `batch_size` different transitions can be sampled from the buffer."""
+        return batch_size + 1 <= self.num_in_buffer
+
+    def _encode_sample(self, idxes):
+        obs_batch      = np.concatenate([self._encode_observation(idx)[None] for idx in idxes], 0)
+        act_batch      = self.action[idxes]
+        rew_batch      = self.reward[idxes]
+        next_obs_batch = np.concatenate([self._encode_observation(idx + 1)[None] for idx in idxes], 0)
+        done_mask      = np.array([1.0 if self.done[idx] else 0.0 for idx in idxes], dtype=np.float32)
+
+        return self.transition(obs_batch, act_batch, rew_batch, next_obs_batch, done_mask)
+
+
+    def sample(self, batch_size):
+        """Sample `batch_size` different transitions.
+        i-th sample transition is the following:
+        when observing `obs_batch[i]`, action `act_batch[i]` was taken,
+        after which reward `rew_batch[i]` was received and subsequent
+        observation  next_obs_batch[i] was observed, unless the epsiode
+        was done which is represented by `done_mask[i]` which is equal
+        to 1 if episode has ended as a result of that action.
+        Parameters
+        ----------
+        batch_size: int
+            How many transitions to sample.
+        Returns
+        -------
+        obs_batch: np.array
+            Array of shape
+            (batch_size, img_h, img_w, img_c * frame_history_len)
+            and dtype np.uint8
+        act_batch: np.array
+            Array of shape (batch_size,) and dtype np.int32
+        rew_batch: np.array
+            Array of shape (batch_size,) and dtype np.float32
+        next_obs_batch: np.array
+            Array of shape
+            (batch_size, img_h, img_w, img_c * frame_history_len)
+            and dtype np.uint8
+        done_mask: np.array
+            Array of shape (batch_size,) and dtype np.float32
+        """
+        assert self.can_sample(batch_size)
+        idxes = sample_n_unique(lambda: random.randint(0, self.num_in_buffer - 2), batch_size)
+        return self._encode_sample(idxes)
+
+    def encode_recent_observation(self):
+        """Return the most recent `frame_history_len` frames.
+        Returns
+        -------
+        observation: np.array
+            Array of shape (img_h, img_w, img_c * frame_history_len)
+            and dtype np.uint8, where observation[:, :, i*img_c:(i+1)*img_c]
+            encodes frame at time `t - frame_history_len + i`
+        """
+        assert self.num_in_buffer > 0
+        return self._encode_observation((self.next_idx - 1) % self.size)
+
+    def _encode_observation(self, idx):
+        end_idx   = idx + 1 # make noninclusive
+        start_idx = end_idx - self.frame_history_len
+        # this checks if we are using low-dimensional observations, such as RAM
+        # state, in which case we just directly return the latest RAM.
+        if len(self.obs.shape) == 2:
+            return self.obs[end_idx-1]
+        # if there weren't enough frames ever in the buffer for context
+        if start_idx < 0 and self.num_in_buffer != self.size:
+            start_idx = 0
+        for idx in range(start_idx, end_idx - 1):
+            if self.done[idx % self.size]:
+                start_idx = idx + 1
+        missing_context = self.frame_history_len - (end_idx - start_idx)
+        # if zero padding is needed for missing context
+        # or we are on the boundry of the buffer
+        if start_idx < 0 or missing_context > 0:
+            frames = [np.zeros_like(self.obs[0]) for _ in range(missing_context)]
+            for idx in range(start_idx, end_idx):
+                frames.append(self.obs[idx % self.size])
+            return np.concatenate(frames, 2)
+        else:
+            # this optimization has potential to saves about 30% compute time \o/
+            img_h, img_w = self.obs.shape[1], self.obs.shape[2]
+            return self.obs[start_idx:end_idx].transpose(1, 2, 0, 3).reshape(img_h, img_w, -1)
+
+    def store_frame(self, frame):
+        """Store a single frame in the buffer at the next available index, overwriting
+        old frames if necessary.
+        Parameters
+        ----------
+        frame: np.array
+            Array of shape (img_h, img_w, img_c) and dtype np.uint8
+            the frame to be stored
+        Returns
+        -------
+        idx: int
+            Index at which the frame is stored. To be used for `store_effect` later.
+        """
+        if self.obs is None:
+            self.obs      = np.empty([self.size] + list(frame.shape), dtype=np.uint8)
+            self.action   = np.empty([self.size],                     dtype=np.int32)
+            self.reward   = np.empty([self.size],                     dtype=np.float32)
+            self.done     = np.empty([self.size],                     dtype=np.bool)
+        self.obs[self.next_idx] = frame
+
+        ret = self.next_idx
+        self.next_idx = (self.next_idx + 1) % self.size
+        self.num_in_buffer = min(self.size, self.num_in_buffer + 1)
+
+        return ret
+
+    def store_effect(self, idx, action, reward, done):
+        """Store effects of action taken after obeserving frame stored
+        at index idx. The reason `store_frame` and `store_effect` is broken
+        up into two functions is so that once can call `encode_recent_observation`
+        in between.
+        Paramters
+        ---------
+        idx: int
+            Index in buffer of recently observed frame (returned by `store_frame`).
+        action: int
+            Action that was performed upon observing this frame.
+        reward: float
+            Reward that was received when the actions was performed.
+        done: bool
+            True if episode was finished after performing that action.
+        """
+        self.action[idx] = action
+        self.reward[idx] = reward
+        self.done[idx]   = done
+        
+####################################################################################################
+######################################## DM Atari Wrappers #########################################
+####################################################################################################
+
+class NoopResetEnv(gym.Wrapper):
+    def __init__(self, env=None, noop_max=30):
+        """Sample initial states by taking random number of no-ops on reset.
+        No-op is assumed to be action 0.
+        """
+        super(NoopResetEnv, self).__init__(env)
+        self.noop_max = noop_max
+        assert env.unwrapped.get_action_meanings()[0] == 'NOOP'
+
+    def _reset(self):
+        """ Do no-op action for a number of steps in [1, noop_max]."""
+        self.env.reset()
+        noops = np.random.randint(1, self.noop_max + 1)
+        for _ in range(noops):
+            obs, _, _, _ = self.env.step(0)
+        return obs
+
+class FireResetEnv(gym.Wrapper):
+    def __init__(self, env=None):
+        """Take action on reset for environments that are fixed until firing."""
+        super(FireResetEnv, self).__init__(env)
+        assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
+        assert len(env.unwrapped.get_action_meanings()) >= 3
+
+    def _reset(self):
+        self.env.reset()
+        obs, _, _, _ = self.env.step(1)
+        obs, _, _, _ = self.env.step(2)
+        return obs
+
+class EpisodicLifeEnv(gym.Wrapper):
+    def __init__(self, env=None):
+        """Make end-of-life == end-of-episode, but only reset on true game over.
+        Done by DeepMind for the DQN and co. since it helps value estimation.
+        """
+        super(EpisodicLifeEnv, self).__init__(env)
+        self.lives = 0
+        self.was_real_done  = True
+        self.was_real_reset = False
+
+    def _step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self.was_real_done = done
+        # check current lives, make loss of life terminal,
+        # then update lives to handle bonus lives
+        lives = self.env.unwrapped.ale.lives()
+        if lives < self.lives and lives > 0:
+            # for Qbert somtimes we stay in lives == 0 condtion for a few frames
+            # so its important to keep lives > 0, so that we only reset once
+            # the environment advertises done.
+            done = True
+        self.lives = lives
+        return obs, reward, done, info
+
+    def _reset(self):
+        """Reset only when lives are exhausted.
+        This way all states are still reachable even though lives are episodic,
+        and the learner need not know about any of this behind-the-scenes.
+        """
+        if self.was_real_done:
+            obs = self.env.reset()
+            self.was_real_reset = True
+        else:
+            # no-op step to advance from terminal/lost life state
+            obs, _, _, _ = self.env.step(0)
+            self.was_real_reset = False
+        self.lives = self.env.unwrapped.ale.lives()
+        return obs
+
+class MaxAndSkipEnv(gym.Wrapper):
+    def __init__(self, env=None, skip=4):
+        """Return only every `skip`-th frame"""
+        super(MaxAndSkipEnv, self).__init__(env)
+        # most recent raw observations (for max pooling across time steps)
+        self._obs_buffer = deque(maxlen=2)
+        self._skip       = skip
+
+    def _step(self, action):
+        total_reward = 0.0
+        done = None
+        for _ in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            self._obs_buffer.append(obs)
+            total_reward += reward
+            if done:
+                break
+
+        max_frame = np.max(np.stack(self._obs_buffer), axis=0)
+
+        return max_frame, total_reward, done, info
+
+    def _reset(self):
+        """Clear past frame buffer and init. to first obs. from inner env."""
+        self._obs_buffer.clear()
+        obs = self.env.reset()
+        self._obs_buffer.append(obs)
+        return obs
+
+def _process_frame84(frame):
+    img = np.reshape(frame, [210, 160, 3]).astype(np.float32)
+    img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
+    img = Image.fromarray(img)
+    resized_screen = img.resize((84, 110), Image.BILINEAR)
+    resized_screen = np.array(resized_screen)
+    x_t = resized_screen[18:102, :]
+    x_t = np.reshape(x_t, [84, 84, 1])
+    return x_t.astype(np.uint8)
+
+class ProcessFrame84(gym.Wrapper):
+    def __init__(self, env=None):
+        super(ProcessFrame84, self).__init__(env)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(84, 84, 1))
+
+    def _step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        return _process_frame84(obs), reward, done, info
+
+    def _reset(self):
+        return _process_frame84(self.env.reset())
+
+class ClippedRewardsWrapper(gym.Wrapper):
+    def _step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        return obs, np.sign(reward), done, info
+
+def wrap_deepmind_ram(env):
+    env = EpisodicLifeEnv(env)
+    env = NoopResetEnv(env, noop_max=30)
+    env = MaxAndSkipEnv(env, skip=4)
+    if 'FIRE' in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
+    env = ClippedRewardsWrapper(env)
+    return env
+
+def wrap_deepmind(env):
+    assert 'NoFrameskip' in env.spec.id
+    env = EpisodicLifeEnv(env)
+    env = NoopResetEnv(env, noop_max=30)
+    env = MaxAndSkipEnv(env, skip=4)
+    if 'FIRE' in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
+    env = ProcessFrame84(env)
+    #env = ClippedRewardsWrapper(env)
+    return env
