a17b22c63a19b3951ff4bbf4a166a94f92806d6a
AlexGrinch <grinchuk.alexey@gmail.com>
2018-04-22 14:11:28 +0300

    doom env fix

--- doom/environments.py
+++ doom/environments.py
@@ -9,10 +9,10 @@ import matplotlib.pyplot as plt
 
 class DoomEnvironment:
     
-    def __init__(self, scenario):
+    def __init__(self, scenario, path_to_config="doom"):
         self.game = DoomGame()
-        self.game.load_config("doom/"+scenario+".cfg")
-        self.game.set_doom_scenario_path("doom/"+scenario+".wad")
+        self.game.load_config(path_to_config+"/"+scenario+".cfg")
+        self.game.set_doom_scenario_path(path_to_config+"/"+scenario+".wad")
         self.game.set_window_visible(False)
         self.game.init()
         self.num_actions = len(self.game.get_available_buttons())
@@ -36,8 +36,7 @@ class DoomEnvironment:
         return img.astype(np.uint8)
     
     def preprocess_obs(self, obs):
-        img = obs
-        img = np.rollaxis(img, 0, 3)
+        img = np.rollaxis(obs, 0, 3)
         img = np.reshape(img, [self.h, self.w, 3]).astype(np.float32)
         img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
         img = Image.fromarray(img)
@@ -47,11 +46,25 @@ class DoomEnvironment:
     
     def action_to_doom(self, a):
         action = [0 for i in range(self.num_actions)]
-        action[a] = 1
+        action[int(a)] = 1
         return action
         
     def step(self, a):
-        pass
+        action = self.action_to_doom(a)
+        reward = self.game.make_action(action)
+        
+        done = self.game.is_episode_finished()
+        
+        if done:
+            new_obs = np.zeros_like(self.current_obs, dtype=np.uint8)
+        else:
+            game_state = self.game.get_state() 
+            new_obs = game_state.screen_buffer
+            new_obs = self.preprocess_obs(new_obs)
+
+        self.current_obs = new_obs
+        
+        return self.get_obs(), reward, done    
     
     def watch_random_play(self, max_ep_length=1000, frame_skip=4):
         self.reset()
@@ -70,40 +83,17 @@ class DoomEnvironment:
 
 class DoomBasic(DoomEnvironment):
     
-    def __init__(self):
-        super(DoomBasic, self).__init__(scenario="basic")
-        
-    def reset(self):
-        self.game.new_episode()
-        game_state = self.game.get_state() 
-        obs = game_state.screen_buffer
-        self.h, self.w = obs.shape[1:3]
-        self.current_obs = self.preprocess_obs(obs)
-        return self.get_obs()    
-        
-    def step(self, a):
-        action = self.action_to_doom(a)
-        reward = self.game.make_action(action)
-        
-        done = self.game.is_episode_finished()
-        
-        if done:
-            new_obs = np.zeros_like(self.current_obs, dtype=np.uint8)
-        else:
-            game_state = self.game.get_state() 
-            new_obs = game_state.screen_buffer
-            new_obs = self.preprocess_obs(new_obs)
-
-        self.current_obs = new_obs
-        
-        return self.get_obs(), reward, done                              
+    def __init__(self, path_to_config="doom"):
+        super(DoomBasic, self).__init__(scenario="basic", 
+                                        path_to_config=path_to_config)                          
                           
 ################################## Defend the line ##################################
 
 class DoomDefendTheLine(DoomEnvironment):
     
-    def __init__(self):
-        super(DoomDefendTheLine, self).__init__(scenario="defend_the_line")
+    def __init__(self, path_to_config="doom"):
+        super(DoomDefendTheLine, self).__init__(scenario="defend_the_line",
+                                                path_to_config=path_to_config)
         
     def step(self, a):
         action = self.action_to_doom(a)
@@ -133,8 +123,9 @@ class DoomDefendTheLine(DoomEnvironment):
 
 class DoomDefendTheCenter(DoomEnvironment):
     
-    def __init__(self):
-        super(DoomDefendTheCenter, self).__init__(scenario="defend_the_center")
+    def __init__(self, path_to_config="doom"):
+        super(DoomDefendTheCenter, self).__init__(scenario="defend_the_center", 
+                                                  path_to_config=path_to_config)
         
     def step(self, a):
         action = self.action_to_doom(a)
