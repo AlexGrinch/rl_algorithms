5fd7ed14544cf4502fc85bc47816b34a0b1a00e9
AlexGrinch <grinchuk.alexey@gmail.com>
2018-07-28 14:51:27 +0300

    added envs

---
+++ environments/doom/README.md
@@ -0,0 +1,80 @@
+# Description
+
+This is a gym-like wrapper over [VizDoom](http://vizdoom.cs.put.edu.pl) environment for deep reinforcement learning research. To install **vizdoom** python library, please follow the instructions from [VizDoom GitHub repository](https://github.com/mwydmuch/ViZDoom).
+
+## How to use
+
+```python
+from environments.doom.doom_env import DoomBasic
+
+# create environment instance
+env = DoomBasic()
+
+# reset the environment and get preprocessed 84x84x1 observation
+obs = env.reset()
+
+# make step in the environment
+next_obs, reward, done = env.step(action)
+
+# get original RGB image of current observation
+rgb_obs = env.get_obs_rgb()
+```
+
+# Available scenarios
+
+Here is the list of available scenarios (copied and adapted from [here](https://github.com/mwydmuch/ViZDoom/blob/master/scenarios/README.md)):
+
+|![doom1](img/basic.png) | ![doom2](img/def_center.png) | ![doom3](img/def_line.png) 
+|:---:|:---:|:---:|
+|**Basic**|**Defend the center**|**Defend the line**|
+|![doom4](img/health.png) | ![doom5](img/predict.png) | ![doom6](img/corridor.png)
+|**Health gathering**|**Predict the position**|**Deadly corridor**|
+
+## Basic
+
+Map is a rectangle with gray walls, ceiling and floor. Player is spawned along the longer wall, in the center. A red, circular monster is spawned randomly somewhere along the opposite wall. Player can only go left/right and shoot. 1 hit is enough to kill the monster. Episode finishes when monster is killed or on timeout.
+
+- **Actions:** move left, move right, shoot
+- **Rewards:** +100 for killing the monster; -6 for missing; -1 otherwise
+- **Episode termination:** monster is killed or after 300 time steps
+
+## Defend the center
+
+Map is a large circle. Player is spawned in the exact center. 5 melee-only, monsters are spawned along the wall. Monsters are 
+killed after a single shot. After dying each monster is respawned after some time. Ammo is limited to 26 bullets. Episode ends when the player dies or on timeout.
+
+- **Actions:** turn left, turn right, shoot
+- **Rewards:** +1 for killing the monster; -0.1 for missing; -0.1 for losing health; -1 for death; 0 otherwise
+- **Episode termination:** player is killed or after 2100 time steps
+
+## Defend the line
+
+Map is a rectangle. Player is spawned along the longer wall, in the center. 3 melee-only and 3 shooting monsters are spawned along the oposite wall. Monsters are killed after a single shot, at first. After dying each monster is respawned after some time and can endure more damage. Episode ends when the player dies or on timeout.
+
+- **Actions:** move left, move right, turn left, turn right, shoot
+- **Rewards:** +1 for killing the monster; -0.1 for missing; -0.1 for losing health; -1 for death; 0 otherwise
+- **Episode termination:** player is killed or after 2100 time steps
+
+## Health gathering
+
+Map is a rectangle with green, acidic floor which hurts the player periodically. Initially there are some medkits spread uniformly over the map. A new medkit falls from the skies every now and then. Medkits heal some portions of player's health - to survive agent needs to pick them up. Episode finishes after player's death or on timeout.
+
+- **Actions:** turn left, turn right, move forward
+- **Rewards:** -100 for death; +1 otherwise
+- **Episode termination:** player is killed or after 2100 time steps
+
+## Predict position
+
+Map is a rectangle room. Player is spawned along the longer wall, in the center. A monster is spawned randomly somewhere along the opposite wall and walks between left and right corners along the wall. Player is equipped with a rocket launcher and a single rocket. Episode ends when missle hits a wall/the monster or on timeout.
+
+- **Actions:** move left, move right, shoot
+- **Rewards:** +1 for killing the monster; -0.0001 otherwise
+- **Episode termination:** monster is killed or after 300 time steps
+
+## Deadly corridor
+
+Map is a corridor with shooting monsters on both sides (6 monsters in total). A green vest is placed at the oposite end of the corridor. Reward is proportional (negative or positive) to change of the distance between the player and the vest. If player ignores monsters on the sides and runs straight for the vest he will be killed somewhere along the way.
+
+- **Actions:** move left, move right, turn left, turn right, shoot
+- **Rewards:** -100 for death; +dX (-dx) for getting closer (further) to the vest
+- **Episode termination:** player is killed or after 4200 time steps
---
+++ environments/doom/doom_env.py
@@ -0,0 +1,177 @@
+from vizdoom import DoomGame
+from PIL import Image
+import numpy as np
+
+from IPython import display
+import matplotlib.pyplot as plt
+
+########################## Doom environment template class ##########################
+
+class DoomEnvironment:
+    
+    def __init__(self, scenario, path_to_config="doom/config"):
+        self.game = DoomGame()
+        self.game.load_config(path_to_config+"/"+scenario+".cfg")
+        self.game.set_doom_scenario_path(path_to_config+"/"+scenario+".wad")
+        self.game.set_window_visible(False)
+        self.game.init()
+        self.num_actions = len(self.game.get_available_buttons())
+        
+    def reset(self):
+        self.game.new_episode()
+        game_state = self.game.get_state() 
+        obs = game_state.screen_buffer
+        self.h, self.w = obs.shape[1:3]
+        self.current_obs = self.preprocess_obs(obs)
+        if self.game.get_available_game_variables_size() == 2:
+            self.ammo, self.health = game_state.game_variables
+        return self.get_obs()
+    
+    def get_obs(self):
+        return self.current_obs[:, :, None]
+    
+    def get_obs_rgb(self):
+        img = self.game.get_state().screen_buffer
+        img = np.rollaxis(img, 0, 3)
+        img = np.reshape(img, [self.h, self.w, 3])
+        return img.astype(np.uint8)
+    
+    def preprocess_obs(self, obs):
+        img = np.rollaxis(obs, 0, 3)
+        img = np.reshape(img, [self.h, self.w, 3]).astype(np.float32)
+        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
+        img = Image.fromarray(img)
+        img = img.resize((84, 84), Image.BILINEAR)
+        img = np.array(img)
+        return img.astype(np.uint8)
+    
+    def action_to_doom(self, a):
+        action = [0 for i in range(self.num_actions)]
+        action[int(a)] = 1
+        return action
+        
+    def step(self, a):
+        action = self.action_to_doom(a)
+        reward = self.game.make_action(action)
+        
+        done = self.game.is_episode_finished()
+        
+        if done:
+            new_obs = np.zeros_like(self.current_obs, dtype=np.uint8)
+        else:
+            game_state = self.game.get_state() 
+            new_obs = game_state.screen_buffer
+            new_obs = self.preprocess_obs(new_obs)
+
+        self.current_obs = new_obs
+        
+        return self.get_obs(), reward, done    
+    
+    def watch_random_play(self, max_ep_length=1000, frame_skip=4):
+        self.reset()
+        for i in range(max_ep_length):
+            a = np.random.randint(self.num_actions)
+            obs, reward, done = self.step(a)
+            if done: break
+                
+            img = self.get_obs_rgb()
+            if i % frame_skip == 0:
+                plt.imshow(img)
+                display.clear_output(wait=True)
+                display.display(plt.gcf())
+
+####################################### Basic #######################################                         
+
+class DoomBasic(DoomEnvironment):
+    
+    def __init__(self, path_to_config="doom/config"):
+        super(DoomBasic, self).__init__(scenario="basic", 
+                                        path_to_config=path_to_config)                          
+                          
+################################## Defend the line ##################################
+
+class DoomDefendTheLine(DoomEnvironment):
+    
+    def __init__(self, path_to_config="doom/config"):
+        super(DoomDefendTheLine, self).__init__(scenario="defend_the_line",
+                                                path_to_config=path_to_config)
+        
+    def step(self, a):
+        action = self.action_to_doom(a)
+        reward = self.game.make_action(action)
+        
+        done = self.game.is_episode_finished()
+        
+        if done:
+            new_obs = np.zeros_like(self.current_obs, dtype=np.uint8)
+        else:
+            game_state = self.game.get_state() 
+            new_obs = game_state.screen_buffer
+            new_obs = self.preprocess_obs(new_obs)
+            new_ammo, new_health = game_state.game_variables
+            
+            if (reward == 1.0): reward += 0.1
+            if (new_ammo < self.ammo): reward -= 0.1
+            if (new_health < self.health): reward -= 0.1
+                
+            self.ammo, self.health = new_ammo, new_health
+
+        self.current_obs = new_obs
+        
+        return self.get_obs(), reward, done
+    
+################################# Defend the center #################################
+
+class DoomDefendTheCenter(DoomEnvironment):
+    
+    def __init__(self, path_to_config="doom/config"):
+        super(DoomDefendTheCenter, self).__init__(scenario="defend_the_center", 
+                                                  path_to_config=path_to_config)
+        
+    def step(self, a):
+        action = self.action_to_doom(a)
+        reward = self.game.make_action(action)
+        
+        done = self.game.is_episode_finished()
+        
+        if done:
+            new_obs = np.zeros_like(self.current_obs, dtype=np.uint8)
+        else:
+            game_state = self.game.get_state() 
+            new_obs = game_state.screen_buffer
+            new_obs = self.preprocess_obs(new_obs)
+            new_ammo, new_health = game_state.game_variables
+            
+            if (reward == 1.0): reward += 0.1
+            if (new_ammo < self.ammo): reward -= 0.1
+            if (new_health < self.health): reward -= 0.1
+                
+            self.ammo, self.health = new_ammo, new_health
+
+        self.current_obs = new_obs
+        
+        return self.get_obs(), reward, done
+
+############################### Predict the position ################################ 
+
+class DoomPredictThePosition(DoomEnvironment):
+    
+    def __init__(self, path_to_config="doom/config"):
+        super(DoomPredictThePosition, self).__init__(scenario="predict_position",
+                                                     path_to_config=path_to_config)   
+        
+############################### Predict the position ################################ 
+
+class DoomHealthGathering(DoomEnvironment):
+    
+    def __init__(self, path_to_config="doom/config"):
+        super(DoomHealthGathering, self).__init__(scenario="health_gathering",
+                                                  path_to_config=path_to_config) 
+        
+############################### Predict the position ################################ 
+
+class DoomDeadlyCorridor(DoomEnvironment):
+    
+    def __init__(self, path_to_config="doom/config"):
+        super(DoomDeadlyCorridor, self).__init__(scenario="deadly_corridor",
+                                                 path_to_config=path_to_config) 
